8d8f84f88449641280a2bc5ee767a36833983135
Aresguerre <bacem.benachour@esprit.tn>
2015-04-03 15:21:11 +0100

    #contact height ++

--- index.php
+++ index.php
@@ -21,11 +21,11 @@
 		<nav>
 		    <div class="nav-wrapper blue-grey darken-3">
 			    <ul id="nav-mobile" class="left hide-on-med-and-down">
-				    <li><a class=" active" href="#home" data="home">Home</a></li>
+				    <li><a class="active" href="#home" data="home">Home</a></li>
 				    <li><a class="" href="#produits" data ="produits">Produits</a></li>
-				    <li><a href="#services" data="services">Service</a></li>
-				    <li><a class=" " href="#about" data="about">About</a></li>
-				    <li><a class=" " href="#contact" data="contact">Contact</a></li>
+				    <li><a class="" href="#services" data="services">Service</a></li>
+				    <li><a class="" href="#about" data="about">About</a></li>
+				    <li><a class="" href="#contact" data="contact">Contact</a></li>
 			    </ul>
 			    <ul id="nav-mobile" class="right hide-on-med-and-down">
 				    <li><a href="#modal1" data="signin" class="modal-trigger waves-effect waves-light modal-trigger"> <i class="ion-log-in small"></i> Sign In</a></li>
