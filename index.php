da451e7308eaf06d45d3d6edd58336395109a1f4
BoBrebel <mohamedelfadhel.elhachemi@esprit.tn>
2015-04-05 22:58:00 +0100

    same shit different file

--- index.php
+++ index.php
@@ -321,12 +321,27 @@
   	<footer class="page-footer blue-grey darken-3">
         <div class="container">
             <div class="row">
-                <div class="col l6 s12">
-                	<h5 class="white-text">Footer Content</h5>
-                	<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <div class="col l4 white-text">
+                	<h5 class="white-text">Main Navigation</h5>
+                <ul id="footer-nav" class="left">
+				    <li><a class="active" href="#home" data="home">Home</a></li>
+				    <li><a class="" href="#produits" data ="produits">Produits</a></li>
+				    <li><a class="" href="#services" data="services">Service</a></li>
+				    <li><a class="" href="#about" data="about">About</a></li>
+				    <li><a class="" href="#contact" data="contact">Contact</a></li>
+			    </ul>
                 </div>
-            	<div class="col l4 offset-l2">
-                	<h5 class="white-text">Links</h5>
+                <div class="col l4">
+                	<h5 class="white-text">Site Links</h5>
+                <ul id="site-links" class="left">
+                	<li><a href="#">Terms of service</a></li>
+                	<li><a href="#">Privacy policy</a></li>
+                	<li><a href="#">+216 xx xx xx xx</a></li>
+                </ul>
+
+                </div>
+            	<div class="col l4">
+                	<h5 class="white-text">Social</h5>
                 	
                   	<div class="row social">
                   		<div class="col l2">
@@ -335,14 +350,17 @@
                   		<div class="col l2">
                   			<a href="#"><i class="ion-social-facebook large"></i></a>
                   		</div>
+                  		<div class="col l2">
+                  			<a href="#"><i class="ion-social-rss large"></i></a>
+                  		</div>
                   	</div>
             	</div>
             </div>
          </div>
         <div class="footer-copyright">
             <div class="container">
-            	© 2014 Copyright Text
-            	<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            	© 2015 Copyright Team Unicorns
+            	
             </div>
         </div>
     </footer>
