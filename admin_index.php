4d9e7ab51844fc4b7256ce7517bca28d86786d57
saif00 <seifeddine.oueslati@esprit.tn>
2015-04-20 16:23:16 +0100

    admin page

---
+++ admin_index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/td/html4/strict.">
+
+<html>
+<head>
+	<title>Admin</title>
+	<meta charset="utf-8">
+	
+	<link rel="stylesheet" href="css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="Style/Style.css">
+	<link rel="stylesheet" type="text/css" href="Style/Animation.css">
+	<link rel="stylesheet" type="text/css" href="css/ionicons.min.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/Smooth_scroll.js"></script>
+	<script type="text/javascript" src="js/slideshow.js"></script>
+	<script src="js/materialize.min.js"></script>
+	<script type="text/javascript" src="js/modals.js"></script>
+	
+</head>
+<body>
+
+		<div id="vmenu"> 
+            <ul> 
+              <li><a href="">Users</a></li> 
+              <li><a href="">Products</a></li> 
+              <li><a href="">Ticket</a></li> 
+              <li><a href="">Forum</a></li>
+            </ul>   
+        </div>
+	
+
+</body>
+</html>
