2f91158a8a8ae07f20867456709af101330ea400
saif00 <seifeddine.oueslati@esprit.tn>
2015-04-20 21:23:08 +0100

    admin

--- admin/admin_product.php
+++ admin/admin_product.php
@@ -5,15 +5,15 @@
 	<title>Admin</title>
 	<meta charset="utf-8">
 	
-	<link rel="stylesheet" href="css/materialize.min.css">
-	<link rel="stylesheet" type="text/css" href="Style/Style.css">
-	<link rel="stylesheet" type="text/css" href="Style/Animation.css">
-	<link rel="stylesheet" type="text/css" href="css/ionicons.min.css">
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/Smooth_scroll.js"></script>
-	<script type="text/javascript" src="js/slideshow.js"></script>
-	<script src="js/materialize.min.js"></script>
-	<script type="text/javascript" src="js/modals.js"></script>
+	<link rel="stylesheet" href="../css/materialize.min.css">
+  <link rel="stylesheet" type="text/css" href="../Style/Style.css">
+  <link rel="stylesheet" type="text/css" href="../Style/Animation.css">
+  <link rel="stylesheet" type="text/css" href="../css/ionicons.min.css">
+  <script type="text/javascript" src="../js/jquery.js"></script>
+  <script type="text/javascript" src="../js/Smooth_scroll.js"></script>
+  <script type="text/javascript" src="../js/slideshow.js"></script>
+  <script src="../js/materialize.min.js"></script>
+  <script type="text/javascript" src="../js/modals.js"></script>
 	
 </head>
 <body>
