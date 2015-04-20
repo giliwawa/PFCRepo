28a39c1ef966a05de25380c0192187bf02553129
saif00 <seifeddine.oueslati@esprit.tn>
2015-04-20 14:57:42 +0100

    admin sign in

---
+++ admin_signin.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/td/html4/strict.">
+
+<html>
+<head>
+	<title>Admin</title>
+	<meta charset="utf-8">
+	
+	<link rel="stylesheet" href="css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="Style/Style.css">
+	<link rel="stylesheet" type="text/css" href="Style/Animation.css">
+	<link rel="stylesheet" type="text/css" href="css/ionicons.min.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/Smooth_scroll.js"></script>
+	<script type="text/javascript" src="js/slideshow.js"></script>
+	<script src="js/materialize.min.js"></script>
+	<script type="text/javascript" src="js/modals.js"></script>
+	
+</head>
+<body>
+
+	<section id="a_signin">
+		<div class="container">
+			<form >
+				<br>
+  				<div class="row">
+  					<div class="input-field col s12">
+  						<i class="mdi-action-account-circle prefix"></i>
+  						<input id="icon_prefix" type="text" class="validate">
+  						<label for="icon_prefix">Pseudo</label>
+  					</div>
+  					<div class="input-field col s12">
+  						<i class="mdi-action-lock prefix"></i>
+  						<input id="icon_password" type="password" class="validate">
+  						<label for="icon_password">Mot de Passe</label>
+  					</div>
+  				</div>
+  				<br>
+  				<br>
+  				<div class="row">
+  					<center>
+  						<input id="login" type="submit" class="btn cyan darken-2" value="Sign In">
+  					</center>
+  				</div>
+  			</form>
+		</div>
+	</section>
+
+</body>
+</html>
