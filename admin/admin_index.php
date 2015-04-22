b76df4140ce62e21f4fffcb113c67bb291b9c33f
Aresguerre <bacem.benachour@esprit.tn>
2015-04-22 21:26:02 +0100

    arrange Style with comments

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -4,7 +4,8 @@
 <head>
 	<title>Admin</title>
 	<meta charset="utf-8">
-	
+
+	<link rel="stylesheet" type="text/css" href="../css/ionicons.min.css">
 	<link rel="stylesheet" href="../css/materialize.min.css">
 	<link rel="stylesheet" type="text/css" href="../Style/Style.css">
 	<link rel="stylesheet" type="text/css" href="../Style/Animation.css">
