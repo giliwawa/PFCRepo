e275b81509d441384f72037d1a504f90418b34da
giliwawa <alaa.ksontini@esprit.tn>
2015-04-23 01:11:03 +0200

    footer.php
    seperating footer

---
+++ view/footer.php
@@ -0,0 +1,49 @@
+<footer class="page-footer blue-grey darken-3">
+        <div class="container">
+            <div class="row">
+                <div class="col l4 white-text">
+                	<h5 class="white-text">Main Navigation</h5>
+                <ul id="footer-nav" class="left">
+				    <li><a class="active" href="#home" data="home">Home</a></li>
+				    <li><a class="" href="#produits" data ="produits">Produits</a></li>
+				    <li><a class="" href="#services" data="services">Service</a></li>
+				    <li><a class="" href="#about" data="about">About</a></li>
+				    <li><a class="" href="#contact" data="contact">Contact</a></li>
+			    </ul>
+                </div>
+                <div class="col l4">
+                	<h5 class="white-text">Site Links</h5>
+                <ul id="site-links" class="left">
+                	<li><a href="#">Terms of service</a></li>
+                	<li><a href="#">Privacy policy</a></li>
+                	<li><a href="#">+216 xx xx xx xx</a></li>
+                </ul>
+
+                </div>
+            	<div class="col l4">
+                	<h5 class="white-text">Social</h5>
+                	
+                  	<div class="row social">
+                  		<div class="col l2">
+                  			<a href="#"><i class="ion-social-twitter large"></i></a>
+                  		</div>
+                  		<div class="col l2">
+                  			<a href="#"><i class="ion-social-facebook large"></i></a>
+                  		</div>
+                  		<div class="col l2">
+                  			<a href="#"><i class="ion-social-rss large"></i></a>
+                  		</div>
+                  	</div>
+            	</div>
+            </div>
+         </div>
+        <div class="footer-copyright">
+            <div class="container">
+            	© 2015 Copyright Team Unicorns
+            	
+            </div>
+        </div>
+    </footer>
+    
+</body>
+</html>
