7c093dba26169cb990cf7d875e8db03692c0326d
BoBrebel <mohamedelfadhel.elhachemi@esprit.tn>
2015-04-24 00:42:37 +0100

    Products WIP 2

--- model/produitModel.php
+++ model/produitModel.php
@@ -22,6 +22,19 @@
                 $i++;   
             }
             return $tableau;
+        }
+        public function afficher_categorie($cat){
+            $tableau = array();
+            $querry = "SELECT * FROM produit WHERE categorie=".$categorie"";
+            $resultat = mysql_query($query) or die("erreur".mysql_error());
+            $i=0;
+            while ($data = mysql_fetch_array($resultat)) {
+                 # code...
+                $Pr = new produit($data);
+                $tableau[$i] = $Pr;
+                $i++;
+             } 
+             return $tableau;
         }        
         
         
