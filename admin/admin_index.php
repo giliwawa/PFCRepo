7eb54fbaa6ff0e82d915ae5ff9766cb6e5cc3183
saif00 <seifeddine.oueslati@esprit.tn>
2015-04-23 13:55:13 +0100

    amin index

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -20,29 +20,33 @@
 <body id="bg_admin">
 	<section id="admin">
 		<div class="container">
-			<h4>administration interface</h4>
-			<center><h6>User</h6></center>
-			<ul class="collapsible" data-collapsible="accordion">
-			    <li>
-			      <div class="collapsible-header"><i class="mdi-action-account-child"></i><a href="admin_index.php">Users</a></div>
-			    </li>
-			    <li>
-			      <div class="collapsible-header"><i class="mdi-action-shopping-cart"></i>Products</div>
-			      <div class="collapsible-body">
-			      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>Guitar</div>
-			      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>Percution</div>
-			      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>A vent</div>
-			      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>Electronique</div>
-			      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>accessoir</div>
-			      </div>
-			    </li>
-			    <li>
-			      <div class="collapsible-header"><i class="mdi-action-description"></i><a href="admin_ticket.php">Ticket</a></div>
-			    </li>
-			    <li>
-			      <div class="collapsible-header"><i class="mdi-communication-forum"></i><a href="admin_forum.php">Forum</a></div>
-			    </li>
-	  		</ul>
+			<div class="row">
+				<div class="col s3 sidebar">
+					<h4>administration interface</h4>
+					<center><h6>User</h6></center>
+					<ul class="collapsible" data-collapsible="accordion">
+					    <li>
+					      <div class="collapsible-header"><i class="mdi-action-account-child"></i><a href="admin_index.php">Users</a></div>
+					    </li>
+					    <li>
+					      <div class="collapsible-header"><i class="mdi-action-shopping-cart"></i>Products</div>
+					      <div class="collapsible-body">
+					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>Guitar</div>
+					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>Percution</div>
+					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>A vent</div>
+					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>Electronique</div>
+					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>accessoir</div>
+					      </div>
+					    </li>
+					    <li>
+					      <div class="collapsible-header"><i class="mdi-action-description"></i><a href="admin_ticket.php">Ticket</a></div>
+					    </li>
+					    <li>
+					      <div class="collapsible-header"><i class="mdi-communication-forum"></i><a href="admin_forum.php">Forum</a></div>
+					    </li>
+			  		</ul>
+				</div>
+			</div>
 		</div>
 	</section>
         
