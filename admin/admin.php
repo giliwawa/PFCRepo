6c223340d7390f0019ae7f264a885f06fa148208
Aresguerre <bacem.benachour@esprit.tn>
2015-04-24 19:48:18 +0100

    admin

---
+++ admin/admin.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Admin Interface</title>
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="../view/css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="../view/Style/Style.css">
+	<link rel="stylesheet" type="text/css" href="../view/css/ionicons.min.css">
+	
+	<script type="text/javascript" src="../view/js/jquery.js"></script>
+	<script type="text/javascript" src="../view/js/materialize.min.js"></script>
+	<script type="text/javascript" src="../view/js/modals.js"></script>
+	<script type="text/javascript" src="../view/js/sidenav.js"></script>
+
+</head>
+<body>
+
+</body>
+</html>
