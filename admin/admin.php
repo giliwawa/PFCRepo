a1ffb9949ef9daec9c7af8889d4c7dc9b6cdcc0b
Aresguerre <bacem.benachour@esprit.tn>
2015-04-25 03:16:32 +0100

    Ajax on wheel

--- admin/admin.php
+++ admin/admin.php
@@ -11,50 +11,55 @@
 	<script type="text/javascript" src="../view/js/materialize.min.js"></script>
 	<script type="text/javascript" src="../view/js/modals.js"></script>
 	<script type="text/javascript" src="../view/js/sidenav.js"></script>
-
+	<script type="text/javascript" src="../view/js/loadTicket.js"></script>
 </head>
 <body>
-	<nav class="top-nav">
-        <div class="container">
-          <div class="nav-wrapper">
-          	<a class="page-title">Administrator Platform</a>
-          </div>
-        </div>
-      </nav>
-  <ul id="nav-mobil" class="side-nav fixed left">
-    <li class="bold"><a href="" class="waves-effect waves-teal">Home</a></li>
-    <li class="no-padding bold">
-      <ul class="collapsible collapsible-accordion">
-            <li><a class="collapsible-header waves-effect waves-teal">Users</a>
-              <div class="collapsible-body">
-                <ul>
-                  <li><a href="">Display Users</a></li>
-                  <li><a href="">Add Users</a></li>
-                  <li><a href="">Modify Users</a></li>
-                  <li><a href="">Delete Users</a></li>
-                </ul>
-              </div>
-            </li>
-            <li><a class="collapsible-header waves-effect waves-teal">Products</a>
-              <div class="collapsible-body">
-                <ul>
-                  <li><a href="">Display Products</a></li>
-                  <li><a href="">Add Products</a></li>
-                  <li><a href="">Modify Products</a></li>
-                  <li><a href="">Delete Products</a></li>
-                  <li><a href="">Add Discount</a></li>
-                </ul>
-              </div>
-            </li>   
-        </ul>
-    </li>
-    <li class="bold"><a href="" class="waves-effect waves-teal">Tickets</a></li>
-    <li class="bold"><a href="" class="waves-effect waves-teal">Statistics</a></li>
+	<div class="navigation_bars">
+		<nav class="top-nav">
+	        <div class="container">
+	          <div class="nav-wrapper">
+	          	<a class="page-title">Administrator Platform</a>
+	          </div>
+	        </div>
+	    </nav>
+	  	<ul id="nav-mobil" class="side-nav fixed left">
+		    <li class="bold"><a href="" class="waves-effect waves-teal">Home</a></li>
+		    <li class="no-padding bold">
+		      <ul class="collapsible collapsible-accordion">
+		            <li><a class="collapsible-header waves-effect waves-teal">Users</a>
+		              <div class="collapsible-body">
+		                <ul>
+		                  <li><a href="">Display Users</a></li>
+		                  <li><a href="">Add Users</a></li>
+		                  <li><a href="">Modify Users</a></li>
+		                  <li><a href="">Delete Users</a></li>
+		                </ul>
+		              </div>
+		            </li>
+		            <li><a class="collapsible-header waves-effect waves-teal">Products</a>
+		              <div class="collapsible-body">
+		                <ul>
+		                  <li><a href="">Display Products</a></li>
+		                  <li><a href="">Add Products</a></li>
+		                  <li><a href="">Modify Products</a></li>
+		                  <li><a href="">Delete Products</a></li>
+		                  <li><a href="">Add Discount</a></li>
+		                </ul>
+		              </div>
+		            </li>   
+		        </ul>
+		    </li>
+		    <li class="bold"><a class="waves-effect waves-teal" onclick="loadTicket();">Tickets</a></li>
+		    <li class="bold"><a href="" class="waves-effect waves-teal">Statistics</a></li>
 
-
-
-  </ul>
-  <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+		</ul>
+	  <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+	</div>
+	<div id="show">
+		<br>
+		<br>
+		<br>
+	</div>
 
 
 </body>
--- admin/admin_ticket.php
+++ admin/admin_ticket.php
@@ -3,83 +3,27 @@ require_once '../model/ticketModel.php';
 $ticketModel=new ticketModel();
 $listeT = $ticketModel->displayTicket(1);
 ?>
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/td/html4/strict.">
-
-<html>
-<head>
-	<title>Admin</title>
-	<meta charset="utf-8">
-	
-	<link rel="stylesheet" href="../css/materialize.min.css">
-  <link rel="stylesheet" type="text/css" href="../Style/Style.css">
-  <link rel="stylesheet" type="text/css" href="../Style/Animation.css">
-  <link rel="stylesheet" type="text/css" href="../css/ionicons.min.css">
-  <script type="text/javascript" src="../js/jquery.js"></script>
-  <script type="text/javascript" src="../js/Smooth_scroll.js"></script>
-  <script type="text/javascript" src="../js/slideshow.js"></script>
-  <script src="../js/materialize.min.js"></script>
-  <script type="text/javascript" src="../js/modals.js"></script>	
-</head>
-<body id="bg_admin">
-
-<section id="admin">
-	<div class="container ">
-			<div class="row">
-				<div class="col s3 sidebar">
-					<h4>administration interface</h4>
-					<center><h6>Ticket</h6></center>
-					<ul class="collapsible" data-collapsible="accordion">
-					    <li>
-					      <div class="collapsible-header"><i class="mdi-action-account-child"></i><a href="admin_index.php">Users</a></div>
-					    </li>
-					    <li>
-					      <div class="collapsible-header"><i class="mdi-action-shopping-cart"></i>Products</div>
-					      <div class="collapsible-body">
-					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>Guitar</div>
-					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>Percution</div>
-					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>A vent</div>
-					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>Electronique</div>
-					      	<div class="collapsible-header"><i class="mdi-social-whatshot"></i>accessoir</div>
-					      </div>
-					    </li>
-					    <li>
-					      <div class="collapsible-header"><i class="mdi-action-description"></i><a href="admin_ticket.php">Ticket</a></div>
-					    </li>
-					    <li>
-					      <div class="collapsible-header"><i class="mdi-communication-forum"></i><a href="admin_forum.php">Forum</a></div>
-					    </li>
-			  		</ul>
-				</div>
-				<div class="col s9">
-					<table>
-						<thead>
-							<tr>
-								<th>id</th>
-								<th>Name</th>
-								<th>Email</th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php
-								foreach ($listeT as $Tick) :
-							?>
-							<tr>
-									<td><?php echo $Tick->getid_ticket() ;?></td>
-									<td><?php echo $Tick->getname() ;?></td>
-									<td><?php echo $Tick->getemail() ;?></td>
-									<td><a href="ConsulterTicket.php?IdTicket=<?php echo $Tick->getid_ticket(); ?>">View</a></td>
-							</tr>
-							<?php
-								endforeach;
-							?>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	</section>
-
-</body>
-</html>
+<table>
+	<thead>
+		<tr>
+			<th>id</th>
+			<th>Name</th>
+			<th>Email</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+<?php
+	foreach ($listeT as $Tick) :
+?>
+		<tr>
+			<td><?php echo $Tick->getid_ticket() ;?></td>
+			<td><?php echo $Tick->getname() ;?></td>
+			<td><?php echo $Tick->getemail() ;?></td>
+			<td><a href="ConsulterTicket.php?IdTicket=<?php echo $Tick->getid_ticket(); ?>">View</a></td>
+		</tr>
+<?php
+	endforeach;
+?>
+	</tbody>
+</table>
