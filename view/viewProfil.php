b1bb976f0f9062eee54f777772f4d00ebc13d270
saif00 <seifeddine.oueslati@esprit.tn>
2015-04-26 15:38:58 +0100

    Profil html

---
+++ view/viewProfil.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/td/html4/strict.">
+
+<?php include 'header.php'; ?>
+	<title>Profil</title>
+</head>
+<body>
+	<?php include 'navbar.php'; ?>
+	<section id="profil">
+		<div class="container">
+			<div class="row">
+				<div class="col s4 img">
+					<img src="">
+				</div>
+				<div class="col s7 titre">
+					<center><h4>Pseudo</h4></center>
+					<br>
+					<p>Nom</p>
+					<p>Prenom</p>
+					<p>Age</p>
+					<a class="waves-effect waves-light btn blue-grey darken-3">
+						<i class="mdi-editor-mode-edit left"></i>Update
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s3 favoris">
+					<center><h4>Favoris</h4></center>
+				</div>
+				<div class="col s8 suggestion">
+					<h4>Orders</h4>
+				</div>
+			</div>
+		</div>
+	</section>
+
+</body>
+</html>
