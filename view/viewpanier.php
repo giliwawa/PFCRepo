2d156ffc3b04627e03988dcf3879a5c3857cdc59
giliwawa <alaa.ksontini@esprit.tn>
2015-04-29 03:27:19 +0200

    wip cart

---
+++ view/viewpanier.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'header.php';
+?>
+</head>
+<?php
+include_once 'navbar.php';
+
+session_start();
+if(isset($_SESSION['id_produit'])){
+    
+}
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
