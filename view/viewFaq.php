4d9cff0a2691abe7cda199cd52a77868395ca9fe
saif00 <seifeddine.oueslati@esprit.tn>
2015-05-13 02:51:57 +0100

    FAQ

--- view/viewFaq.php
+++ view/viewFaq.php
@@ -12,6 +12,34 @@
 
 <section id="faq">
 	<div class="container">
+		<h5>-How to pay?</h5>
+		<p>Our primary way of payment is online payment by MDinar credit cards.
+
+For our customers in Germany, Netherlands, Belgium, France, Denmark, Sweden and Norway we also offer other payment says "offline" (eg by bank transfer).
+
+If you make a payment "offline", the time taken for your web space will be ready a little longer. Regarding this subject, thank you refer to the section "How long will my web space active, if I order now?"</p>
+		<h5>-Do you offer a secure payment via your website?</h5>
+		<p>Yes, it is very secure.
+
+An online payment by credit card is done atravers secure connection, encrypted in 128 bit. We do not save or store any data on your credit card, it means that it is impossible to find / recreate the data you entered in our system when you order online.</p>
+		<h5>-How long will be my web space active, if I order now?</h5>
+		<p>Most paid by credit card orders are processed immediately.
+
+If you choose to pay by a method "offline", it will be necessary to send us the signed domain registration form, so we can process your order. When you place your order you will receive an email with confirmation of your order. In this email you will also find the link to the domain registration form.
+
+In some cases we will ask you to return the signed domain registration form even if you have chosen to pay online via credit card. This may be due to problems of verification of your payment and this is a security measure quite normal. However, if you are asked to return a signed domain registration form, this does not necessarily mean that there is a problem with your credit card payment.</p>
+		<h5>-Is your system stable?</h5>
+		<p>Despite the competitive price, we did not compromise on quality. We only use suppliers whose quality does more to prove and we have a high-performance backup system. Our servers, our server room and our network are monitored 24/24 365 days of the year.
+
+All our servers are Hewlett-Packard servers of high quality with redundant power supplies, mirrored SCSI disks and tape backups daily. Our systems are hosted in a local private servers at Interxion in Denmark. The local server is equipped with a redundant electrical circuit, battery backup, diesel generators and a smoke detector VESDA (Very Early Smoke Detection Alarm).
+
+Our fiber optic backbone network has a redundant infrastructure. Currently, our Internet access (bandwidth) is 2 x 2 Giga byte multi-homed connected and connected to three international carriers, Global Crossing, Deutsche Telecom and Level3, and to Danish Internet eXchange.</p>
+		<h5>-Will I get a refund if I am not satisfied?</h5>
+		<p>If you do not clench completely satisfied with our solution, we guarantee refund your payment within 15 days.
+
+This rebate is valid from the day you place your order via our website and covers all customers and some of the cause of discontent
+
+Money Back Guarantee: If a purchase multiple domains, we would provide you a full refund for the canceled order first. For additional orders, we will refund all but the annual domain fee, you can find them at www.StellarInstrument.com.</p>
 	</div>
 </section>
 
